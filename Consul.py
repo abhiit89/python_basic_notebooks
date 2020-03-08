942c60584571394c9317fc7e59cb046d68856c1a
abhinav-cleo <agupta@cleo.com>
2020-03-08 22:35:51 +0530

    Update Consul.py

--- Consul.py
+++ Consul.py
@@ -1,6 +1,6 @@
 import consul
 # 100.21.190.28
-consul_server = consul.Consul(host='44.229.106.99', port=8500)
+consul_server = consul.Consul(host='127.0.0.1', port=8500)
 consul_kv = consul_server.kv.get(key='', recurse=True)
 # print(consul_kv)
 for item in consul_kv[1]:
