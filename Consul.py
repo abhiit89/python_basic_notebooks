0ed12fa2fe4188c4f689b920bacd2764676f5897
Abhinav Gupta <agupta@cleo.com>
2020-03-08 22:34:48 +0530

    Initial Commit

--- Consul.py
+++ Consul.py
@@ -1,5 +1,6 @@
 import consul
-consul_server = consul.Consul(host='127.0.0.1', port=8500)
+# 100.21.190.28
+consul_server = consul.Consul(host='44.229.106.99', port=8500)
 consul_kv = consul_server.kv.get(key='', recurse=True)
 # print(consul_kv)
 for item in consul_kv[1]:
